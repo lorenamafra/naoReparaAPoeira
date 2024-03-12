50b8affa5b63f4de3e0c3de35b2ce2aaa8a071d3
Denis <denisls02@Outlook.com>
2024-03-11 21:56:26 -0300

    inicio formulário produto

--- BackOffice/FrontEnd/src/Pages/CadastrarProdutos.jsx
+++ BackOffice/FrontEnd/src/Pages/CadastrarProdutos.jsx
@@ -1,7 +1,46 @@
-function CadastrarProdutos() {
-	const user = JSON.parse(sessionStorage.getItem("User"));
+import { InputQuantidade } from "../Styles/CadastrarProdutos.styles";
+import { FormContainer, MainFormContainer } from "../Styles/Form.styles";
 
-	return <div>{user.nome}</div>;
+function CadastrarProdutos() {
+  const handleAdicionarImagem = () => {
+    // Handle adicionar imagem irá retornar um fieldset como o que foi feito, e irá adicionar um sempre que tiver uma imagem nova a ser adicionada
+    // Conte a quantidade de fieldsets
+    // usar a KEY do MAP para ser a quantidade da imagem no nome "imagem[i]"
+  };
+
+  return (
+    <MainFormContainer>
+      <label htmlFor=""> Disco </label>
+      <input type="text" />
+      <label htmlFor=""> Artista </label>
+      <input type="text" />
+      <label htmlFor=""> Genero </label>
+      <input type="text" />
+      <div>
+        <label htmlFor=""> Quantidade </label>
+        <br />
+        <button> - </button>
+        <InputQuantidade type="number" />
+        <button> + </button>
+      </div>
+
+      <div id="inputImagem">
+        <fieldset>
+          <input type="file" name="imagem[0]" />
+          <div>
+            <input type="radio" name="principal" value="true" checked />
+          </div>
+        </fieldset>
+        <button
+          type="button"
+          id="btnNovaImagem"
+          onClick={handleAdicionarImagem}
+        >
+          Inlucir nova imagem
+        </button>
+      </div>
+    </MainFormContainer>
+  );
 }
 
 export default CadastrarProdutos;
