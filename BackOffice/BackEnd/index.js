85167f396ff605c1cf9a2074668c4dbc2d9b2d31
Lorena Mafra <lorena.mafran@gmail.com>
2024-03-20 18:06:30 -0300

    Create: Rota para listar 10 itens por página

--- BackOffice/BackEnd/index.js
+++ BackOffice/BackEnd/index.js
@@ -17,243 +17,270 @@ app.use(jsonParser);
 
 // setting up mysql connection with express
 var connection = mysql.createConnection({
-	host: "127.0.0.1",
-	user: "root",
-	password: "80085",
-	database: "nrp",
-	port: "3306",
+  host: "127.0.0.1",
+  user: "root",
+  password: "80085",
+  database: "nrp",
+  port: "3306",
 });
 
 // Rota para pegar todos os usuários
 
 app.get("/usuarios", function (req, res) {
-	connection.query("SELECT * FROM usuario", (err, rows) => {
-		if (err) throw err;
+  connection.query("SELECT * FROM usuario", (err, rows) => {
+    if (err) throw err;
 
-		res.send({ usuarios: rows });
-	});
+    res.send({ usuarios: rows });
+  });
 });
 
 //Rota para pegar UM usuário
 
 app.post("/usuario", (req, res) => {
-	const email = req.body.email;
-	connection.query(
-		`SELECT * FROM usuario WHERE email = "${email}"`,
+  const email = req.body.email;
+  connection.query(
+    `SELECT * FROM usuario WHERE email = "${email}"`,
 
-		(err, result) => {
-			if (err) {
-				throw err;
-			}
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
 
-			if (result && result.length > 0) {
-				res.send(result[0]);
-			} else {
-				res.status(404).send("Email não encontrado");
-			}
-		}
-	);
+      if (result && result.length > 0) {
+        res.send(result[0]);
+      } else {
+        res.status(404).send("Email não encontrado");
+      }
+    }
+  );
 });
 
 // Rota para Login
 app.post("/usuario/login", (req, res) => {
-	const email = req.body.email;
-	const senha = req.body.senha;
-	connection.query(
-		"SELECT * FROM usuario WHERE email = ?",
-		email,
-		(err, result) => {
-			if (err) {
-				res.send({ err: err });
-			}
-			if (result) {
-				if (result.length > 0) {
-					console.log(result);
-					bcrypt.compare(senha, result[0].senha, (err, response) => {
-						if (err) {
-							console.log(err);
-						}
-						if (response) {
-							res.send(true);
-						}
-					});
-				} else {
-					res.send({
-						message: "Usuário inexistente",
-					});
-				}
-			}
-		}
-	);
+  const email = req.body.email;
+  const senha = req.body.senha;
+  connection.query(
+    "SELECT * FROM usuario WHERE email = ?",
+    email,
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result) {
+        if (result.length > 0) {
+          console.log(result);
+          bcrypt.compare(senha, result[0].senha, (err, response) => {
+            if (err) {
+              console.log(err);
+            }
+            if (response) {
+              res.send(true);
+            }
+          });
+        } else {
+          res.send({
+            message: "Usuário inexistente",
+          });
+        }
+      }
+    }
+  );
 });
 
 // Rota para cadastrar usuário
 app.post("/usuario/cadastrar", (req, res) => {
-	const nome = req.body.nome;
-	const cpf = req.body.cpf;
-	const email = req.body.email;
-	const senha = req.body.senha;
-	const grupo = req.body.grupo;
-	console.log(req.body.email);
-	bcrypt.hash(senha, saltRounds, (err, hash) => {
-		if (err) {
-			res.send({ err: err });
-		}
-		connection.query(
-			`INSERT INTO usuario (nome, cpf, email, senha, status_cliente, grupo) values ('${nome}','${cpf}' , '${email}', '${hash}', "Ativo", '${grupo}')`,
+  const nome = req.body.nome;
+  const cpf = req.body.cpf;
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const grupo = req.body.grupo;
+  console.log(req.body.email);
+  bcrypt.hash(senha, saltRounds, (err, hash) => {
+    if (err) {
+      res.send({ err: err });
+    }
+    connection.query(
+      `INSERT INTO usuario (nome, cpf, email, senha, status_cliente, grupo) values ('${nome}','${cpf}' , '${email}', '${hash}', "Ativo", '${grupo}')`,
 
-			(err, result) => {
-				if (err) {
-					res.send(err);
-				}
+      (err, result) => {
+        if (err) {
+          res.send(err);
+        }
 
-				if (result) {
-					res.send("Conta criada com sucesso!");
-				}
-			}
-		);
-	});
+        if (result) {
+          res.send("Conta criada com sucesso!");
+        }
+      }
+    );
+  });
 });
 
 //Rota para alterar usuário
 app.put("/usuario/alterar", (req, res) => {
-	const email = req.body.email;
-	const grupo = req.body.grupo;
-	const nome = req.body.nome;
-	const senha = req.body.senha;
-	const status = req.body.status;
-	const cpf = req.body.cpf;
+  const email = req.body.email;
+  const grupo = req.body.grupo;
+  const nome = req.body.nome;
+  const senha = req.body.senha;
+  const status = req.body.status;
+  const cpf = req.body.cpf;
 
-	bcrypt.hash(senha, saltRounds, (err, hash) => {
-		connection.query(
-			`UPDATE usuario SET GRUPO = '${grupo}', NOME ='${nome}', SENHA ='${hash}', CPF ='${cpf}' WHERE EMAIL = '${email}'`,
-			(err, result) => {
-				if (result.affectedRows > 0) {
-					res.status(200).send("Atualizado com sucesso");
-				} else {
-					res.status(400).send("Impossibilitado de alteração ");
-				}
-			}
-		);
-	});
+  bcrypt.hash(senha, saltRounds, (err, hash) => {
+    connection.query(
+      `UPDATE usuario SET GRUPO = '${grupo}', NOME ='${nome}', SENHA ='${hash}', CPF ='${cpf}' WHERE EMAIL = '${email}'`,
+      (err, result) => {
+        if (result.affectedRows > 0) {
+          res.status(200).send("Atualizado com sucesso");
+        } else {
+          res.status(400).send("Impossibilitado de alteração ");
+        }
+      }
+    );
+  });
 });
 
 app.put("/usuario/alterarStatus", (req, res) => {
-	let status = req.body.status_cliente;
-	let email = req.body.email;
-	connection.query(
-		`UPDATE usuario SET status_cliente = '${status}' WHERE EMAIL = '${email}'`,
-		(err, result) => {
-			if (err) {
-				throw err;
-			}
-			if (result.affectedRows > 0) {
-				res.status(200).send("Atualizado com sucesso");
-			} else {
-				res.status(404).send("Email não encontrado ou nenhum dado atualizado");
-			}
-		}
-	);
+  let status = req.body.status_cliente;
+  let email = req.body.email;
+  connection.query(
+    `UPDATE usuario SET status_cliente = '${status}' WHERE EMAIL = '${email}'`,
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      if (result.affectedRows > 0) {
+        res.status(200).send("Atualizado com sucesso");
+      } else {
+        res.status(404).send("Email não encontrado ou nenhum dado atualizado");
+      }
+    }
+  );
 });
 
 app.post("/usuario/pesquisar", (req, res) => {
-	let nome = req.body.nome;
-	console.log(req.body);
-	connection.query(
-		`SELECT * FROM usuario WHERE nome LIKE '%${nome}%';`,
-		(err, result) => {
-			if (err) {
-				throw err;
-			}
+  let nome = req.body.nome;
+  console.log(req.body);
+  connection.query(
+    `SELECT * FROM usuario WHERE nome LIKE '%${nome}%';`,
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
 
-			res.send(result);
-		}
-	);
+      res.send(result);
+    }
+  );
 });
 
 //CADASTRAR PRODUTOS!!!!!!!!!!!!!!!!!!!!!!!!!!
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, "../FrontEnd/public");
-	},
-	filename: function (req, file, cb) {
-		console.log(file);
-		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-		cb(null, uniqueSuffix + path.extname(file.originalname));
-	},
+  destination: function (req, file, cb) {
+    cb(null, "../FrontEnd/public");
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
 });
 
 const uploadImage = multer({ storage: storage }).array("images", 12);
 app.post("/produto/inserir", uploadImage, (req, res) => {
-	console.log(req.body);
-	let nome_disco = req.body.nome_disco;
-	const estoque = req.body.estoque;
-	const valor = req.body.valor;
-	const artista = req.body.artista;
-	const genero = req.body.genero;
-	const ano = req.body.ano;
-	const avaliacao = req.body.avaliacao;
-	const descricao = req.body.descricao;
+  console.log(req.body);
+  let nome_disco = req.body.nome_disco;
+  const estoque = req.body.estoque;
+  const valor = req.body.valor;
+  const artista = req.body.artista;
+  const genero = req.body.genero;
+  const ano = req.body.ano;
+  const avaliacao = req.body.avaliacao;
+  const descricao = req.body.descricao;
 
-	var abbr = artista
-		.split(" ")
-		.map(function (item) {
-			return item[0];
-		})
-		.join("");
+  var abbr = artista
+    .split(" ")
+    .map(function (item) {
+      return item[0];
+    })
+    .join("");
 
-	nome_disco = nome_disco.replaceAll(" ", "");
+  nome_disco = nome_disco.replaceAll(" ", "");
 
-	cod_produto = abbr.concat(nome_disco, ano);
+  cod_produto = abbr.concat(nome_disco, ano);
 
-	connection.query(
-		`INSERT INTO produto (cod_produto, nome_disco, estoque, valor, artista, genero, ano, avaliacao, status_produto, descricao) values ('${cod_produto}','${nome_disco}', ${estoque}, ${valor}, '${artista}', '${genero}', ${ano}, ${avaliacao}, "Ativo", '${descricao}')`,
-		(err, result) => {
-			if (err) {
-				res.send(err);
-			}
+  connection.query(
+    `INSERT INTO produto (cod_produto, nome_disco, estoque, valor, artista, genero, ano, avaliacao, status_produto, descricao) values ('${cod_produto}','${nome_disco}', ${estoque}, ${valor}, '${artista}', '${genero}', ${ano}, ${avaliacao}, "Ativo", '${descricao}')`,
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
 
-			if (result) {
-				res.send("Produto adicionado com sucesso!");
-			}
-		}
-	);
+      if (result) {
+        res.send("Produto adicionado com sucesso!");
+      }
+    }
+  );
 });
 
 //ALTERAR PRODUTO!!!
 app.put("/produto/alterarProduto", (req, res) => {
-	let cod_produto = req.body.cod_produto;
-	let estoque = req.body.estoque;
-	let valor = req.body.valor;
-	let artista = req.body.artista;
-	let avaliacao = req.body.avaliacao;
-	let descricao = req.body.descricao;
-	let status_produto = req.body.status_produto;
+  let cod_produto = req.body.cod_produto;
+  let estoque = req.body.estoque;
+  let valor = req.body.valor;
+  let artista = req.body.artista;
+  let avaliacao = req.body.avaliacao;
+  let descricao = req.body.descricao;
+  let status_produto = req.body.status_produto;
 
-	connection.query(
-		`UPDATE produto SET estoque = ${estoque}, valor = ${valor}, artista = '${artista}', avaliacao = ${avaliacao}, descricao = '${descricao}', status ='${status_produto} WHERE cod_produto = '${cod_produto}'`,
-		(err, result) => {
-			if (err) {
-				throw err;
-			}
-			if (result.affectedRows > 0) {
-				res.status(200).send("Atualizado com sucesso");
-			} else {
-				res.status(404).send("Nenhum dado atualizado");
-			}
-		}
-	);
+  connection.query(
+    `UPDATE produto SET estoque = ${estoque}, valor = ${valor}, artista = '${artista}', avaliacao = ${avaliacao}, descricao = '${descricao}', status ='${status_produto} WHERE cod_produto = '${cod_produto}'`,
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      if (result.affectedRows > 0) {
+        res.status(200).send("Atualizado com sucesso");
+      } else {
+        res.status(404).send("Nenhum dado atualizado");
+      }
+    }
+  );
 });
 
 app.get("/produtos", function (req, res) {
-	connection.query(
-		"SELECT * FROM produto ORDER BY id_produto DESC",
-		(err, rows) => {
-			if (err) throw err;
+  connection.query(
+    `SELECT * FROM produto ORDER BY id_produto DESC`,
+    (err, rows) => {
+      if (err) throw err;
 
-			res.send({ produtos: rows });
-		}
-	);
+      res.send({ produtos: rows });
+    }
+  );
+});
+
+// listar os 10 primeiros itens da tabela (use essa rota só na página 1)
+app.get("/produtos", function (req, res) {
+  connection.query(`SELECT * FROM produto LIMIT 10`, (err, rows) => {
+    if (err) throw err;
+
+    res.send({ produtos: rows });
+  });
+});
+
+// listar os itens do 11 ao 20 (use essa rota só na página 2)
+app.get("/produtos", function (req, res) {
+  connection.query(`SELECT * FROM produto LIMIT 10 OFFSET 20`, (err, rows) => {
+    if (err) throw err;
+
+    res.send({ produtos: rows });
+  });
+});
+
+// listar os itens do 21 ao 30 (use essa rota só na página 3)
+app.get("/produtos", function (req, res) {
+  connection.query(`SELECT * FROM produto LIMIT 10 OFFSET 30`, (err, rows) => {
+    if (err) throw err;
+
+    res.send({ produtos: rows });
+  });
 });
 
 // app.put("/alterar-nome", (req, res) => {
@@ -363,5 +390,5 @@ app.get("/produtos", function (req, res) {
 // });
 
 app.listen(port, (req, res) => {
-	console.log(`Server listening to port ${port}, BACK OFFICE`);
+  console.log(`Server listening to port ${port}, BACK OFFICE`);
 });
