38424b7438611b22f46d34038b3b25e9ad8a829c
Lorena Mafra <lorena.mafran@gmail.com>
2024-04-19 17:51:05 -0300

    Correção na importação

--- FrontOffice/FrontEnd/src/pages/LandingPage.jsx
+++ FrontOffice/FrontEnd/src/pages/LandingPage.jsx
@@ -1,21 +1,18 @@
 import Lancamentos from "../Components/Lancamentos";
-
-import MaisVendidos from "../Components/MaisVendidos";
-import Promocoes from "../Components/Promocoes";
 import Footer from "../Components/Footer";
 import Nav from "../Components/Nav";
 import { MainLandingPageContainer } from "../styles/LandingPage.styles";
 
 function LandingPage() {
-	return (
-		<MainLandingPageContainer>
-			<Nav />
-			<Lancamentos />
-			{/* <MaisVendidos />
+  return (
+    <MainLandingPageContainer>
+      <Nav />
+      <Lancamentos />
+      {/* <MaisVendidos />
 			<Promocoes /> */}
-			<Footer />
-		</MainLandingPageContainer>
-	);
+      <Footer />
+    </MainLandingPageContainer>
+  );
 }
 
 export default LandingPage;
