b9160bfe8296f1a67667046a20e4fe58da525b55
tallesjr <118635024+tallesjr@users.noreply.github.com>
2024-04-24 00:46:41 -0300

    Criação de tela de frete e styles
    Criação de tela de frete e styles

---
+++ FrontOffice/FrontEnd/src/styles/CalculoFrete.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MainFreteContainer = styled.div``;
+
+export const CalculoFretePage = styled.div``;
+
+export const EnderecoEntregaContainer = styled.div``;
+
+export const OpcoesFreteContainer = styled.div``;
+
+export const OpFretediv = styled.div``;
+
+export const ResumoFreteContainer = styled.div``;
+
+export const ButtonContinuar = styled.button``;
